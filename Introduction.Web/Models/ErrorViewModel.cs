90ec2b2c78cc7b8a9aa44f129848c05c46134b19
Ivan Cesar <ivan.cesar@splitit.com>
2024-03-01 10:10:22 +0100

    Pokazna vjezba - jednostavni primjer

---
+++ Introduction.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Introduction.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
