247549320964f1782753e959d1cd434ca414bc1d
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2024-03-15 12:45:15 +0100

    Task [14]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,46 @@
 {
     class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var a = 10;
+            Task zadatak1 = Task.Run(() =>
+            {
+                Console.WriteLine("Zadatak 1 započeo");
+                Thread.Sleep(1000);
+                Console.WriteLine("Zadatak 1 završio");
+            });
+            Console.WriteLine("Čekam zadatak 1..");
 
+
+            Task zadatak2 = Task.Run(() =>
+            {
+                Console.WriteLine("Zadatak 2 započeo");
+                Thread.Sleep(1500);
+                Console.WriteLine("Zadatak 2 završio");
+            });
+            Console.WriteLine("Čekam zadatak 2..");
+
+            Task.WaitAll(zadatak1, zadatak2);
+
+
+            /*
+            OUTPUT SA Task.WaitAll() --> Čeka se da svi zadatci završe
+            Čekam zadatak 1..
+            Zadatak 1 započeo
+            Čekam zadatak 2..
+            Zadatak 2 započeo
+            Zadatak 1 završio
+            Zadatak 2 završio
+            */
+
+            /*
+            OUTPUT SA Task.WaitAny() --> Čeka se da jedan zadatka završe
+            Čekam zadatak 1..
+            Zadatak 1 započeo
+            Čekam zadatak 2..
+            Zadatak 2 započeo
+            Zadatak 1 završio
+            */
 
         }
 
