eb97229e56194c6269025f4d5b49f55df9a8a66e
Vedran-Josipovic <vedranjosipovic.posao@gmail.com>
2024-03-15 13:21:43 +0100

    Task [15]

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,50 +2,46 @@
 {
     class MainClass
     {
+        static Task zadatak1 = Task.Run(() =>
+        {
+            Console.WriteLine("Zadatak 1 započeo");
+            Thread.Sleep(1000);
+            Console.WriteLine("Zadatak 1 završio");
+        });
+
+        static Task zadatak2 = Task.Run(() =>
+        {
+            Console.WriteLine("Zadatak 2 započeo");
+            Thread.Sleep(1500);
+            Console.WriteLine("Zadatak 2 završio");
+        });
+
         public static void Main(string[] args)
         {
-            Task zadatak1 = Task.Run(() =>
-            {
-                Console.WriteLine("Zadatak 1 započeo");
-                Thread.Sleep(1000);
-                Console.WriteLine("Zadatak 1 završio");
-            });
-            Console.WriteLine("Čekam zadatak 1..");
+            //Task.WaitAll(zadatak1, zadatak2);
+            //.WaitAny(zadatak1, zadatak2);
 
-
-            Task zadatak2 = Task.Run(() =>
-            {
-                Console.WriteLine("Zadatak 2 započeo");
-                Thread.Sleep(1500);
-                Console.WriteLine("Zadatak 2 završio");
-            });
-            Console.WriteLine("Čekam zadatak 2..");
-
-            Task.WaitAll(zadatak1, zadatak2);
-
-
-            /*
-            OUTPUT SA Task.WaitAll() --> Čeka se da svi zadatci završe
-            Čekam zadatak 1..
-            Zadatak 1 započeo
-            Čekam zadatak 2..
-            Zadatak 2 započeo
-            Zadatak 1 završio
-            Zadatak 2 završio
-            */
-
-            /*
-            OUTPUT SA Task.WaitAny() --> Čeka se da jedan zadatka završe
-            Čekam zadatak 1..
-            Zadatak 1 započeo
-            Čekam zadatak 2..
-            Zadatak 2 započeo
-            Zadatak 1 završio
-            */
+            Task t1 = SleepF1();
+            Console.WriteLine($"Waiting on task SleepF1()..");
+            t1.Wait();
 
         }
 
+        private static async Task SleepF1()
+        {
+            Console.WriteLine($"Sleeping 1 started");
+            await Task.Delay(1000);
+            Console.WriteLine($"SleepF2 called");
+            await SleepF2();
+            Console.WriteLine($"Sleeping 1 completed");
+        }
 
+        private static async Task SleepF2()
+        {
+            Console.WriteLine($"Sleeping 2 started");
+            await Task.Delay(2000);
+            Console.WriteLine($"Sleeping 2 completed");
+        }
 
     }
 }
